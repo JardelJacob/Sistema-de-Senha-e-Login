1d9b0b3c8abb31166ed3c021cd64cf5f639d1c34
Jardel Jacob <jardel_jjacob@hotmail.com>
2016-08-07 10:59:32 -0300

    Configuração do HxPHP

--- app/config.php
+++ app/config.php
@@ -3,6 +3,13 @@
 	$configs = new HXPHP\System\Configs\Config;
 
 	$configs->env->add('development');
+	$configs -> env->development->baseURI ='/Sistema/';
+	$configs -> env -> development->database->setConnectionData(array(
+		'host' => 'localhost',
+		'user'=>'root',
+		'password'=>' ',
+		'dbname'=>'sistemsenha'
+		));
 
 	/*
 		//Globais
