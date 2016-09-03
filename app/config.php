f17f5be94dd4f682ed5b23fa64c613ffac6ce3c9
Jardel <jardel_jjacob@hotmail.com>
2016-09-03 16:37:27 -0300

    Configuração do HXPHP => htaccess e config.php

--- app/config.php
+++ app/config.php
@@ -1,6 +1,16 @@
 <?php
 	//Constantes
 	$configs = new HXPHP\System\Configs\Config;
+	$configs->env->add('development');
+
+	$configs->env->development->baseURI = '/Sistema/';
+
+	$configs->env->development->database->setConnectionData(array(
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => '',
+		'dbname' => 'sistemahx'
+	));
 
 	/*
 		//Globais
@@ -48,6 +58,6 @@
 			'from_mail' => 'email@remetente.com.br'
 		));
 	*/
-	 
+
 
 	return $configs;
